372902f0785ba84ef734e34bff15dbef6f4f791f
Gabriel Aguiar <goaguiar99@gmail.com>
2026-02-21 19:51:59 -0300

    chore: stop tracking generated/settings files

--- veins_qos/out/gcc-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
+++
@@ -1,9 +0,0 @@
-veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
-	veins_inet/VeinsInetSampleMessage.msg \
-	/home/goaguiar/master_veins/inet/src/inet/common/INETDefs.msg \
-	/home/goaguiar/master_veins/inet/src/inet/common/Units.msg \
-	/home/goaguiar/master_veins/inet/src/inet/common/packet/chunk/Chunk.msg
-veins_inet/VeinsInetSampleMessage.msg:
-/home/goaguiar/master_veins/inet/src/inet/common/INETDefs.msg:
-/home/goaguiar/master_veins/inet/src/inet/common/Units.msg:
-/home/goaguiar/master_veins/inet/src/inet/common/packet/chunk/Chunk.msg:
