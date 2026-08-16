618987664dda004175a55e44f363484b78f9e795
Gabriel Aguiar <goaguiar@tractian.com>
2026-08-16 16:58:24 -0300

    Revert "Add predictive VO protection and multi-crasher A/B configs (heavy)."
    This reverts commit 9a92dc1562d8219a5b42f459b813763a2d218578.

--- veins_qos/src/mac/V2xHcf.h
+++ veins_qos/src/mac/V2xHcf.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <map>
-
-#include "inet/linklayer/common/MacAddress.h"
 #include "inet/linklayer/ieee80211/mac/coordinationfunction/Hcf.h"
 
 namespace veins_qos::mac {
@@ -27,27 +24,6 @@ class V2xHcf : public inet::ieee80211::Hcf
     long beGrantSuppressedWhileBlockedCount = 0;
     long voProtectionActivationCount = 0;
 
-    // Predictive periodic protection (optional): learn the crash VO burst
-    // cadence from overheard VO frames and block BE right before the next
-    // predicted burst, instead of only reacting once a copy was decoded.
-    bool predictiveBlocking = false;
-    omnetpp::simtime_t predictiveLead = SIMTIME_ZERO;
-    omnetpp::simtime_t predictiveWindow = SIMTIME_ZERO;
-    omnetpp::simtime_t predictiveMinGap = SIMTIME_ZERO;
-    omnetpp::simtime_t predictiveMinPeriod = SIMTIME_ZERO;
-    omnetpp::simtime_t predictiveMaxPeriod = SIMTIME_ZERO;
-
-    struct VoPredictorTrack {
-        omnetpp::simtime_t lastFrameAt = -1;
-        omnetpp::simtime_t lastBurstAt = -1;
-        omnetpp::simtime_t period = -1;
-        omnetpp::simtime_t preBlockAt = -1;
-    };
-    std::map<inet::MacAddress, VoPredictorTrack> voPredictorTracks;
-    omnetpp::cMessage *predictTimer = nullptr;
-    omnetpp::simsignal_t voPredictiveBlockSignal = omnetpp::simsignal_t();
-    long voPredictiveBlockCount = 0;
-
   protected:
     inet::ieee80211::AccessCategory classifyAccessCategory(const inet::Ptr<const inet::ieee80211::Ieee80211DataOrMgmtHeader>& header) const;
     bool hasBeQueuePressure() const;
@@ -59,9 +35,6 @@ class V2xHcf : public inet::ieee80211::Hcf
     void dropBeWhileBlocked(inet::Packet *packet);
     void maybeRequestChannelAccess(inet::ieee80211::AccessCategory ac);
     void scheduleBeRetry();
-    void feedVoPredictor(const inet::MacAddress& source, omnetpp::simtime_t now);
-    void onPredictTimer();
-    void refreshPredictTimer();
 
     virtual void initialize(int stage) override;
     virtual void finish() override;
